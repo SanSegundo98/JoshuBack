434c4a4ecb3a185600bf97d96f613d4c3d026753
SanSegundo98 <pablosansegundo98@gmail.com>
2024-06-01 23:56:38 +0200

    PUT and DELETE cards, small cleanup

--- src/main/java/com/PSSA/joshu/controladores/CardController.java
+++ src/main/java/com/PSSA/joshu/controladores/CardController.java
@@ -1,7 +1,6 @@
 package com.PSSA.joshu.controladores;
 
 import com.PSSA.joshu.dao.ICardRepository;
-import com.PSSA.joshu.dao.IUserRepository;
 import com.PSSA.joshu.modelos.dto.CardDTO;
 import com.PSSA.joshu.modelos.entidades.Card;
 import com.PSSA.joshu.modelos.entidades.User;
@@ -21,14 +20,11 @@ import java.util.List;
 public class CardController {
 	private final ICardService ICardService;
 	private final ICardRepository ICardRepository;
-	private final IUserRepository IUserRepository;
 
 	@Autowired
-	public CardController ( ICardService ICardService, ICardRepository ICardRepository,
-	                        IUserRepository IUserRepository ) {
+	public CardController ( ICardService ICardService, ICardRepository ICardRepository ) {
 		this.ICardService = ICardService;
 		this.ICardRepository = ICardRepository;
-		this.IUserRepository = IUserRepository;
 	}
 
 	@GetMapping("/{userID}")
@@ -43,10 +39,27 @@ public class CardController {
 
 	@PostMapping("/newCard/{userID}")
 	public ResponseEntity<Card> newCard ( @RequestBody Card newCard, @PathVariable Long userID ) {
-		
 		User user = newCard.getUserID();
 		user.setUserID( userID );
 		newCard.setUserID( user );
 		return new ResponseEntity<>( ICardRepository.save( newCard ), HttpStatus.CREATED );
+	}
+
+	@PutMapping("/editedCard/{cardID}")
+	public ResponseEntity<Card> editedCard ( @RequestBody Card editedCardIn, @PathVariable long cardID ) {
+		return ICardRepository.findById( cardID ).map( existingCard -> {
+			existingCard.setExample( editedCardIn.getExample() );
+			existingCard.setTranslation( editedCardIn.getTranslation() );
+			existingCard.setToLearn( editedCardIn.getToLearn() );
+			return new ResponseEntity<>( existingCard, HttpStatus.OK );
+		} ).orElseGet( () -> new ResponseEntity<>( HttpStatus.NOT_FOUND ) );
+	}
+
+	@DeleteMapping("/deleteCard/{cardID}")
+	public ResponseEntity<Void> deleteCard ( @PathVariable long cardID ) {
+		return ICardRepository.findById( cardID ).map( existingCard -> {
+			ICardRepository.delete( existingCard );
+			return new ResponseEntity<Void>( HttpStatus.NO_CONTENT );
+		} ).orElseGet( () -> new ResponseEntity<>( HttpStatus.NOT_FOUND ) );
 	}
 }
